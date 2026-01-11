b913f342afe923d88a3c700d60218dbb31c6b064
Sanket2412 <khardekarsanket@gmail.com>
2026-01-11 23:01:07 +0530

    added components to index files

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,10 @@
+import { About } from "@/components/About";
 import { Background3D } from "@/components/Background3d";
+import { Contact } from "@/components/Contact";
+import { Footer } from "@/components/Footer";
 import { Hero } from "@/components/Hero";
 import { Navbar } from "@/components/Navbar";
+import { Skills } from "@/components/Skills";
 
 const Index = () => {
   return (
@@ -9,7 +13,11 @@ const Index = () => {
      <Navbar />
       <main className="relative z-10" role="main">
         <Hero />
+        <About />
+        <Skills />
+        <Contact />
       </main>
+      <Footer />
     </div>
   );
 };
