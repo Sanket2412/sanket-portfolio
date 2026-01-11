1b94eba9a2c22019e17e28d085ee46a040355abd
Sanket2412 <khardekarsanket@gmail.com>
2026-01-11 21:16:17 +0530

    [Added] Routing logic with notfound page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-import { ThemeProvider } from './components/Themeprovider'
+import { ThemeProvider } from './components/ThemeProvider'
 import { TooltipProvider } from './components/ui/tooltip'
 import { Toaster } from './components/ui/toaster'
 import { Toaster as Sonner } from "./components/ui/sonner";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NotFound from './pages/NotFound'
+import Index from './pages/Index'
 function App() {
 
   return (
@@ -12,7 +14,12 @@ function App() {
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      Hey building something
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </TooltipProvider>
    </ThemeProvider>
   )
